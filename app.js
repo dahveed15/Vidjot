1e8188680b3e2e77154f4802efa4a781513b41de
David Harris <dahveed15@gmail.com>
2018-03-06 11:39:44 -0800

    add partial

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 //initialize application
 const app = express();
@@ -53,6 +55,28 @@ app.use(bodyParser.json());
 
 //Method override middleware
 app.use(methodOverride('_method'));
+
+
+//express-session middleware
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+
+//connect-flash middleware
+app.use(flash());
+
+//Global variables
+app.use(function(req, res, next){
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  //this one will be used for local auth
+  res.locals.error = req.flash('error');
+  next();
+});
 
 
 //ROUTES
@@ -145,6 +169,7 @@ app.post('/ideas', (req, res) => {
     new Idea(newUser)
     .save()
     .then(idea => {
+      req.flash('success_msg', 'Video idea added');
       res.redirect('/ideas');
     });
   }
@@ -164,6 +189,7 @@ app.put('/ideas/:id', (req, res) => {
 
     idea.save()
     .then(() => {
+      req.flash('success_msg', 'Video idea updated');
       res.redirect('/ideas');
     });
   });
@@ -173,6 +199,7 @@ app.put('/ideas/:id', (req, res) => {
 app.delete('/ideas/:id', (req, res) => {
   Idea.remove({_id: req.params.id})
   .then(() => {
+    req.flash('success_msg', 'Video idea removed');
     res.redirect('/ideas');
   });
 });
